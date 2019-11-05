06ad607af041a24395c89d73dfbe07a6c6db3f5d
Gavin Samuels <gavin.samuels10@gmail.com>
2019-11-05 01:27:34 -0500

    adding functionality to select mode on Enter number screen

--- src/screens/HomeGame.tsx
+++ src/screens/HomeGame.tsx
@@ -1,26 +1,94 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Row, Grid } from "react-native-easy-grid";
+import { Button, Segment, Content, Text } from "native-base";
+import ScanTicket from "../components/ScanTicket";
+import EnterNumbers from "../components/EnterNumbers";
 
-export default class Settings extends React.Component {
+export default class HomeGame extends React.Component {
+  state = {
+    activePage: 1
+  };
   static navigationOptions = ({ navigation }) => {
     return {
       title: `${navigation.getParam("game", "Game")}`
     };
   };
+
+  setMode = activePage => () => this.setState({ activePage });
+
+  renderMode = () => {
+    if (this.state.activePage === 1) {
+      return <ScanTicket />;
+    } else {
+      return <EnterNumbers />;
+    }
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Home Game Screen</Text>
-      </View>
+      <Grid>
+        <Row size={1}>
+          <Content padder>
+            <Text style={styles.bodyText}>
+              You can choose to enter your ticket numbers manually or scan your
+              ticket.
+            </Text>
+          </Content>
+        </Row>
+        <Row size={8}>
+          <Segment style={styles.segment}>
+            <Button
+              first
+              active={this.state.activePage === 1}
+              onPress={this.setMode(1)}
+              style={
+                this.state.activePage === 1
+                  ? styles.activeSegment
+                  : styles.inactiveSegment
+              }
+            >
+              <Text>Scan Ticket</Text>
+            </Button>
+            <Button
+              last
+              active={this.state.activePage === 2}
+              onPress={this.setMode(2)}
+              style={
+                this.state.activePage === 2
+                  ? styles.activeSegment
+                  : styles.inactiveSegment
+              }
+            >
+              <Text>Enter Numbers</Text>
+            </Button>
+          </Segment>
+          <View>
+          <Content padder>{this.renderMode()}</Content>
+          </View>
+        </Row>
+      </Grid>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+  bodyText: {
+    fontSize: 14
+  },
+  segment: {
+    backgroundColor: "#fff"
+  },
+  activeSegment: {
+    fontSize: 14,
+    color: "#111",
+    borderBottomColor: "blue",
+    borderBottomWidth: 2
+  },
+  inactiveSegment: {
+    fontSize: 14,
+    borderBottomColor: "blue",
+    borderBottomWidth: 2,
+    color: "#eee"
   }
 });
