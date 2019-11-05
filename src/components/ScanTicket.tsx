00fae170c1a2b642f3b6b539ac20e8421ab02942
Gavin Samuels <gavin.samuels10@gmail.com>
2019-11-05 01:26:03 -0500

    added scan and enter number mock components

---
+++ src/components/ScanTicket.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+export default class ScanTicket extends Component {
+  render() {
+    return (
+      <View>
+        <Text> ScanTicket </Text>
+      </View>
+    );
+  }
+}
